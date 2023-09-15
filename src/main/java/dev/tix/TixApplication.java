692737b343a1aa96ff6b5365c6be717efd1c393b
peeniitii <143002124+peeniitii@users.noreply.github.com>
2023-09-15 14:42:03 +0800

    Add files via upload

---
+++ src/main/java/dev/tix/TixApplication.java
@@ -0,0 +1,13 @@
+package dev.tix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tix/TixApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
